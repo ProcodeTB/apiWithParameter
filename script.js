02844bca1e085390110890be654fbc24ef699a7a
ProcodeTB <113685149+ProcodeTB@users.noreply.github.com>
2025-11-06 17:52:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
 
+fetch('data.json')
+  .then(res => res.json())
+  .then(data => {
+    const result = data.find(item => item.id == id);
+    document.body.innerText = JSON.stringify(result);
+  });
